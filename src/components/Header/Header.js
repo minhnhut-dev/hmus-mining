f3f38c5104b3563c971bd18964342598561f41fe
Minh Nhut dev <nhutpham@advn.vn>
2023-08-30 14:43:09 +0700

    update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,59 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import "./Header.css";
 import { IconButton, Link } from "components";
 import { CartContext } from "App";
-import ModalCart from "components/ModalCart";
+import "../ModalCart/modal.css";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const {cartItems} =  useContext(CartContext);
+  
+  const closeModal = () => {
+    setIsOpen(false); 
+  };
+
+  console.log(cartItems);
 
   return (
     <header className="header">
       <h1>
         <Link to="/">foodhouse</Link>
       </h1>
-      <div>
-        <IconButton icon="icon-shopping-cart" className="header__button" data-bs-toggle="modal" data-bs-target="#exampleModal">
+      <div onClick={() => setIsOpen(!isOpen)}>
+        <IconButton icon="icon-shopping-cart" className="header__button" >
           {!!cartItems && (
-            <span className="header__cart-items">{cartItems.length || 0}</span>
+            <span className="header__cart-items" >{cartItems.length || 0}</span>
           )}
         </IconButton>
+        {isOpen && 
+                <div id="modal" className="modal">
+                <div className="modal-content">
+                  <span id="close-modal" style={{ cursor: 'pointer', color: '#000', fontSize: '30px', float: 'right'}} onClick={closeModal}>
+                    &times;
+                  </span>
+                  <h2 style={{ cursor: 'pointer', color: '#000', fontSize: '30px'}}>Cart</h2>
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>ID</th>
+                        <th>Price</th>
+                        <th>Image</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                    {cartItems.map((item, index) => (
+                  <tr key={index}>
+                    <td>{item.id}</td>
+                    <td>{item.name || 0}</td>
+                    <td><img src={item.image} style={{width: '70px'}}/></td>
+                  </tr>
+                ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+        }
       </div>
     </header>
   );
